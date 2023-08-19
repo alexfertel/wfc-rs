fd587ce4d6855cbadd8075ba381ce7df0d9d5c5a
Alexander Gonzalez <alexfertel97@gmail.com>
2023-08-19 12:59:42 +0200

    ref(bench): rename c arg to b since it is a Bencher

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,8 +6,8 @@ fn generate_size_2(c: &mut Criterion) {
     let pattern_set = wfc::get_patterns(&image, 2);
     let patterns = pattern_set.iter().collect();
     let solver = wfc::Wfc::new(patterns);
-    c.bench_function("generate pattern size 2", |c| {
-        c.iter(|| solver.generate(black_box(10), black_box(10)))
+    c.bench_function("generate pattern size 2", |b| {
+        b.iter(|| solver.generate(black_box(10), black_box(10)))
     });
 }
 
