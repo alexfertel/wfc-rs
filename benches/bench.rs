06be7e8ff232ca6c90eb6c58d5527ef6477f5a7c
Alexander Gonzalez <alexfertel97@gmail.com>
2023-08-21 18:01:03 +0200

    feat(bench): add bench case for pattern size 3

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,15 +1,33 @@
 use ::wfc;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn generate_size_2(c: &mut Criterion) {
+fn size_2(c: &mut Criterion) {
     let image = image::open("bench_data/red-maze.png").unwrap().to_rgb8();
     let pattern_set = wfc::get_patterns(&image, 2);
     let patterns = pattern_set.iter().collect();
     let solver = wfc::Wfc::new(patterns);
-    c.bench_function("generate pattern size 2", |b| {
-        b.iter(|| solver.generate(black_box(10), black_box(10)))
+
+    let mut group = c.benchmark_group("sample-size-10");
+    group.sample_size(10);
+    group.bench_function("32x32-with-p-size-2", |b| {
+        b.iter(|| solver.generate(black_box(32), black_box(32)))
     });
+    group.finish();
 }
 
-criterion_group!(benches, generate_size_2);
+fn size_3(c: &mut Criterion) {
+    let image = image::open("bench_data/water.png").unwrap().to_rgb8();
+    let pattern_set = wfc::get_patterns(&image, 3);
+    let patterns = pattern_set.iter().collect();
+    let solver = wfc::Wfc::new(patterns);
+
+    let mut group = c.benchmark_group("sample-size-10");
+    group.sample_size(10);
+    group.bench_function("32x32 with p-size 3", |b| {
+        b.iter(|| solver.generate(black_box(32), black_box(32)))
+    });
+    group.finish();
+}
+
+criterion_group!(benches, size_2, size_3);
 criterion_main!(benches);
